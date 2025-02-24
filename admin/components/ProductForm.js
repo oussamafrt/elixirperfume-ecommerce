02c17ad873ab375072e3cf80f46bf484eb99d772
oussamafrt <oussama.ferhat01@gmail.com>
2025-02-24 13:27:24 +0100

    Setup AWS S3 for storing uploaded files

--- admin/components/ProductForm.js
+++ admin/components/ProductForm.js
@@ -7,17 +7,18 @@ export default function ProductForm({
         title:existingTitle,
         description:existingDescription,
         price:existingPrice,
-        images,
+        images:existingImages,
     }) {
     const [title,setTitle] = useState(existingTitle || '');
     const [description,setDescription] = useState(existingDescription || '');
     const [price,setPrice] = useState(existingPrice || '');
+    const [images,setImages] = useState(existingImages || []);
     const [goToProducts,setGoToProducts] = useState(false);
     const router = useRouter();
 
     async function saveProduct(ev) {
         ev.preventDefault();
-        const data = { title, description, price };
+        const data = {title, description, price, images};
         if (_id) {
           // Update existing product
           await axios.put ('/api/products', { ...data, _id });
@@ -38,11 +39,10 @@ export default function ProductForm({
         for (const file of files) {
           data.append('file', file);
         }
-        const res = await fetch('/api/upload', {
-          method: 'POST',
-          body: data,
-        })
-        console.log(res);
+        const res = await axios.post('/api/upload',  data);
+        setImages(oldImages => {
+          return [...oldImages, ...res.data.links];
+        });
       }
     }
 
@@ -56,7 +56,12 @@ export default function ProductForm({
             onChange={ev => setTitle(ev.target.value)}
           />
           <label>Photos</label>
-          <div className="mb-2">
+          <div className="mb-2 flex flex-wrap gap-2">
+            {!!images?.length && images.map(link => (
+              <div key={link} className="h-24">
+                <img src={link} alt="" className="rounded-lg"/>
+              </div>
+            ))}
             <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
@@ -64,9 +69,6 @@ export default function ProductForm({
               <div>Upload</div>
               <input type="file" onChange={uploadImages} className="hidden"/>
             </label>
-            {!images?.length && (
-              <div>No photos in this product</div>
-            )}
           </div>
           <label>Description</label>
           <textarea
--- admin/pages/api/upload.js
+++ admin/pages/api/upload.js
@@ -1,4 +1,8 @@
 import multiparty from 'multiparty';
+import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import fs from 'fs';
+import mime from 'mime-types';
+const bucketName = 'elixirperfume-ecommerce';
 
 export default async function handle(req, res) {
     const form = new multiparty.Form();
@@ -9,10 +13,30 @@ export default async function handle(req, res) {
         });
     });
     console.log('length:', files.file.length);
-    console.log(fields);
-    return res.json('ok');
+    const client = new S3Client({
+        region: 'eu-north-1',
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+    });
+    const links = [];
+    for (const file of files.file) {
+        const ext = file.originalFilename.split('.').pop();
+        const newFileName = Date.now() + '.' + ext;
+        await client.send(new PutObjectCommand({
+            Bucket: bucketName,
+            Key: newFileName,
+            Body: fs.readFileSync(file.path),
+            ACL: 'public-read',
+            ContentType: mime.lookup(file.path),
+        }));
+        const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`;
+        links.push(link);
+    }
+    return res.json({links});
 }
 
 export const config = {
     api: {bodyParser: false},
-}
+};
