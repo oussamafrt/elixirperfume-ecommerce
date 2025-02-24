6aec9473d413a3ddc9a5472462c73bf64fd7b5b1
oussamafrt <oussama.ferhat01@gmail.com>
2025-02-24 10:28:10 +0100

    Added product delete functionality

--- admin/components/ProductForm.js
+++ admin/components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm({
         title:existingTitle,
         description:existingDescription,
         price:existingPrice,
+        images,
     }) {
     const [title,setTitle] = useState(existingTitle || '');
     const [description,setDescription] = useState(existingDescription || '');
@@ -26,9 +27,23 @@ export default function ProductForm({
         }
         setGoToProducts(true);
     }
-
     if (goToProducts) {
         router.push('/products');
+    }
+
+    async function uploadImages (ev) {
+      const files = ev.target?.files;
+      if (files?.length > 0) {
+        const data = new FormData();
+        for (const file of files) {
+          data.append('file', file);
+        }
+        const res = await fetch('/api/upload', {
+          method: 'POST',
+          body: data,
+        })
+        console.log(res);
+      }
     }
 
     return (
@@ -40,6 +55,19 @@ export default function ProductForm({
             value={title} 
             onChange={ev => setTitle(ev.target.value)}
           />
+          <label>Photos</label>
+          <div className="mb-2">
+            <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+            </svg>
+              <div>Upload</div>
+              <input type="file" onChange={uploadImages} className="hidden"/>
+            </label>
+            {!images?.length && (
+              <div>No photos in this product</div>
+            )}
+          </div>
           <label>Description</label>
           <textarea
             placeholder="description" 
