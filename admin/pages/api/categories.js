d827d0574bfb2675f8a66b763297dc5273dadf43
oussamafrt <oussama.ferhat01@gmail.com>
2025-02-25 16:00:01 +0100

    modified category management functionality

--- admin/pages/api/categories.js
+++ admin/pages/api/categories.js
@@ -14,4 +14,10 @@ export default async function handle(req, res) {
         const categoryDoc = await Category.create({name, parent:parentCategory});
         res.json(categoryDoc);
     }
+
+    if (method === 'PUT') {
+        const{name,parentCategory,_id} = req.body;
+        const categoryDoc = await Category.updateOne({_id},{name, parent:parentCategory});
+        res.json(categoryDoc);
+    }
 }
--- admin/pages/categories.js
+++ admin/pages/categories.js
@@ -1,8 +1,10 @@
 import Layout from "@/components/layout";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { set } from "mongoose";
 
 export default function Categories() {
+    const [editedCategory, setEditedCategory] = useState(null);
     const [name, setName] = useState('');
     const[parentCategory, setParentCategory] = useState('');
     const [categories, setCategories] = useState([]);
@@ -16,15 +18,29 @@ export default function Categories() {
     }
     async function saveCategory(ev){
         ev.preventDefault();
-        await axios.post('/api/categories', {name,parentCategory});
+        const data = {name,parentCategory};
+        if (editedCategory) {
+            data._id = editedCategory._id;
+            await axios.put('/api/categories', data);
+            setEditedCategory(null);
+        } else {
+            await axios.post('/api/categories', data);
+        }
         setName('');
         fetchCategories();
+    }
+    function editCategory(category) {
+        setEditedCategory(category);
+        setName(category.name);
+        setParentCategory(category.parent?._id);
     }
 
     return (
         <Layout>
             <h1>Categories</h1>
-            <label>New category name</label>
+            <label>
+                {editedCategory ? `Edit category ${editedCategory.name}` : 'Create new category'}
+            </label>
             <form onSubmit={saveCategory} className="flex gap-1">
                 <input 
                     className="mb-0" 
@@ -48,6 +64,7 @@ export default function Categories() {
                     <tr>
                         <td>Category name</td>
                         <td>Parent category</td>
+                        <td></td>
                     </tr>
                 </thead>
                 <tbody>
@@ -55,6 +72,10 @@ export default function Categories() {
                         <tr>
                             <td>{category.name}</td>
                             <td>{category?.parent?.name}</td>
+                            <td>
+                                <button onClick={() => editCategory(category)} className="btn-primary mr-1">Edit</button>
+                                <button className="btn-primary">Delete</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
