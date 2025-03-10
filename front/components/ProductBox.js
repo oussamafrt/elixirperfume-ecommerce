237550fb41ab23d88f6cb3e495d2ebbb93caa294
oussamafrt <oussama.ferhat01@gmail.com>
2025-03-10 20:38:42 +0100

    Implement product display and handle missing images

---
+++ front/components/ProductBox.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const ProductWrapper = styled.div``;
+
+const WhiteBox = styled.div`
+    background-color: #fff;
+    padding: 20px;
+    height: 150px;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    img{
+        max-width: 100%;
+        max-height: 150px;
+    }
+`;
+
+export default function ProductBox({_id,title,description,price,images}) {
+    return (
+        <ProductWrapper>
+            <WhiteBox>
+                <div>
+                    <img src={images[0]} />
+                </div>
+            </WhiteBox>
+            {title}
+        </ProductWrapper>
+    );
+}
--- front/pages/index.js
+++ front/pages/index.js
@@ -2,13 +2,14 @@ import Header from "@/components/Header";
 import Featured from "@/components/Featured";
 import { Product } from "@/models/Product";
 import { mongooseConnect } from "@/lib/mongoose";
+import NewProducts from "@/components/NewProducts";
 
-export default function HomePage({product}) {
-  console.log(product);
+export default function HomePage({featuredProduct, newProducts}) {
   return (
     <div>
       <Header/>
-      <Featured product={product}/>
+      <Featured product={featuredProduct}/>
+      <NewProducts products={newProducts}/>
     </div>
   );
 }
@@ -16,8 +17,12 @@ export default function HomePage({product}) {
 export async function getServerSideProps() {
   const featuredProductId = '67cef6fdb5e16f5d9e0c2d87';
   await mongooseConnect();
-  const product = await Product.findById(featuredProductId);
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
   return {
-    props: {product: JSON.parse(JSON.stringify(product))},
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
   };
 }
