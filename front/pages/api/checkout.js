847113c46f13a31c772b3140951dde3bfebffc7d
oussamafrt <oussama.ferhat01@gmail.com>
2025-03-16 19:35:48 +0100

    trying to add service payment

--- front/pages/api/checkout.js
+++ front/pages/api/checkout.js
@@ -1,5 +1,7 @@
 import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
 import { Product } from "@/models/Product";
+const stripe = require('stripe')(process.env.STRIPE_SK);
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
@@ -20,12 +22,27 @@ export default async function handler(req, res) {
             line_items.push({
                 quantity,
                 price_data: {
-                    currency : 'euro',
+                    currency : 'eur',
                     product_data: {name: productInfo.title},
                     unit_amount: quantity * productInfo.price,
                 },
             });
         }
     }
-    res.json({line_items});
+    const orderDoc = await Order.create({
+        line_items,name,email,phone,address,postalCode,city,country,paid: false,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items,
+        mode: 'payment',
+        customer_email: email,
+        success_url: process.env.PUBLIC_URL + '/cart?success=1',
+        cancel_url: process.env.PUBLIC_URL + '/cart?canceled=1',
+        metadata: {orderId:orderDoc._id.toString()},
+    })
+
+    res.json({
+        url:session.url
+    })
 }
