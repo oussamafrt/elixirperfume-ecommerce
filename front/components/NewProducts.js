143b111bdfb597742098c2b6188ba21b02c877a6
oussamafrt <oussama.ferhat01@gmail.com>
2025-03-17 09:07:46 +0100

    implement the all products page

--- front/components/NewProducts.js
+++ front/components/NewProducts.js
@@ -1,12 +1,7 @@
 import styled from "styled-components";
 import Center from "./Center";
-import ProductBox from "./ProductBox";
+import ProductsGrid from "./ProductsGrid";
 
-const ProductsGrid = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    gap: 30px;
-`;
 
 const Title = styled.h2`
     font-size: 2rem;
@@ -18,11 +13,7 @@ export default function NewProducts ({products}) {
     return (
         <Center>
             <Title>New Arrivals</Title>
-            <ProductsGrid>
-            {products?.length > 0 && products.map (product => (
-                <ProductBox key={product._id} {...product} />
-            ))}
-        </ProductsGrid>
+            <ProductsGrid products={products} />
         </Center>
     );
 }
