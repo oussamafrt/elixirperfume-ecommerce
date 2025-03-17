e74f463e0b464c83129389200dd8af9da0472aeb
oussamafrt <oussama.ferhat01@gmail.com>
2025-03-17 10:44:23 +0100

    configurate single product page

--- front/pages/products.js
+++ front/pages/products.js
@@ -4,10 +4,8 @@ import { mongooseConnect } from '@/lib/mongoose';
 import { Product } from '@/models/Product';
 import styled from 'styled-components';
 import ProductsGrid from '@/components/ProductsGrid';
+import Title from '@/components/Title';
 
-const Title = styled.h1`
-    font-size: 1.5rem;
-`;
 
 export default function ProductsPage({products}) {
     return (
